e328534a3433b51e776a6b61b5d0ae719876e21e
Gary Hussey <ghussey@bossninja.com>
2012-02-02 22:39:25 -0600

    final updates to events style and markup

--- views/modules/event/agenda.php
+++ views/modules/event/agenda.php
@@ -22,30 +22,5 @@ Calender <strong>Agenda View</strong>
 	<a class="cal-button" href="<?php echo site_url($this->config->item('module_name') . '/calendar'); ?>"><span>Calendar View</span></a>
 </div>
 </h1>
-<?php if( !empty( $eventData ) ): $index=0; $total=count($eventData);?>
-<?php 	foreach( $eventData as $keys => $event ):?>
-	<div class="postings<?=(++$index==$total)?' last':''?>">
-		<div class="a_date">
-			<div class="a_date_text">
-				<div class="day"><?= date('l',strtotime($event->event_date))?></div>
-				<div class="month"><?=date('F d,Y',strtotime($event->event_date))?></div>
-				<div class="time"><?=Time24hFormat_Into_AMPMTime($event->start_time)?> - <?= Time24hFormat_Into_AMPMTime($event->end_time)?></div>
-			</div>
-		</div>
-			
-		<article class="a_detail">
-			<header><h2 class="posting_hdr"><?=$event->name?> </h2></header>
-			<p>
-				<?=$event->description ?> 
-				<div class="a_detail_text01">Sponsors</div>
-				<div class="a_detail_text02"><?=$event->sponsors?></div>
-				<div class="a_detail_text01">View Event on:</div>
-				<div class="a_detail_btn">
-					<a href="<?=CheckHTTP_InURL($event->eventbrite_event_url)?>"><div class="orange_btn">Eventbrite</div></a>
-					<a href="<?=CheckHTTP_InURL($event->facebook_event_url)?>"><div class="facebook_btn">facebook</div></a>
-				</div>
-			</p>
-		</article>
-	</div>
-<?php 	endforeach;?>
-<?php endif;?>
+
+<?=$this->load->view($this->config->item('module_name').'/details', array("events"=>$eventData),true);?>
--- views/modules/event/details.php
+++ views/modules/event/details.php
@@ -1,26 +1,43 @@
+<!-- theme header -->
+<div class="pt_banner">
+	<h3 class="pt_heading">Events</h3>
+	<div class="speedbar">
+		<nav>
+			<ul>
+				<li><a href="<?=site_url()?>">Back to Homepage</a></li>
+			{{navigation:links group="events"}}
+                <li>l</li>
+				<li class="{{class}}"><a href="{{url}}">{{title}}</a></li>
+			{{/navigation:links}}
+			
+            </ul>
+		</nav>
+	</div>
+</div>
+<!-- theme header -->
+
 <?php
 	$total = count($events);
 	$index = 0;
 ?>
-<div class="cal-agenda-list">
 <?php foreach( $events as $keys => $event ):?>
-				<div class="postings<?=(++$index==$total)?' last':''?>">
-					<div class="a_date">
-						<div class="a_date_text">
-							<div class="day"><?= date('l',strtotime($event->event_date))?></div>
-							<div class="month"><?=date('F d,Y',strtotime($event->event_date))?></div>
-							<div class="time"><?=Time24hFormat_Into_AMPMTime($event->start_time)?> - <?= Time24hFormat_Into_AMPMTime($event->end_time)?></div>
+				<div class="agenda-listing<?=(++$index==$total)?' last':''?>">
+					<div class="agenda-date">
+						<div class="agenda-date-wrapper">
+							<div class="agenda-day"><?= date('l',strtotime($event->event_date))?></div>
+							<div class="agenda-month"><?=date('F d,Y',strtotime($event->event_date))?></div>
+							<div class="agenda-time"><?=Time24hFormat_Into_AMPMTime($event->start_time)?> - <?= Time24hFormat_Into_AMPMTime($event->end_time)?></div>
 						</div>
 					</div>
 						
-					<article class="a_detail">
-						<header><h2 class="posting_hdr"><?=$event->name?> </h2></header>
+					<article class="agenda-details">
+						<header><h2 class="agenda-header"><?=$event->name?> </h2></header>
 						<p>
 							<?=$event->description ?> 
-							<div class="a_detail_text01">Sponsors</div>
-							<div class="a_detail_text02"><?=$event->sponsors?></div>
-							<div class="a_detail_text01">View Event on:</div>
-							<div class="a_detail_btn">
+							<div class="agenda-spon-heading">Sponsors</div>
+							<div class="agenda-spon-list"><?=$event->sponsors?></div>
+							<div class="agenda-view-heading">View Event on:</div>
+							<div class="agenda-view-buttons">
 								<a href="<?=CheckHTTP_InURL($event->eventbrite_event_url)?>"><div class="orange_btn">Eventbrite</div></a>
 								<a href="<?=CheckHTTP_InURL($event->facebook_event_url)?>"><div class="facebook_btn">facebook</div></a>
 							</div>
@@ -28,4 +45,3 @@
 					</article>
             	</div>
 <?php endforeach;?>
-</div>
